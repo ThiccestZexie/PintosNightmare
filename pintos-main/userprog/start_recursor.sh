1ebd531dd530469565b32c625d682cee8f53a849
Daniel Alchasov <danal315@student.liu.se>
2025-03-09 15:02:12 +0100

    start_recursor.sh now works. Stars recursor. Currently fails att putting the recursor into the filesystem

--- pintos-main/userprog/start_recursor.sh
+++ pintos-main/userprog/start_recursor.sh
@@ -1,4 +1,2 @@
 make -j
-time pintos -v -m 128 -k -T 60 qemu --filesys-size=4 \
-    -p ../examples/recursor_ng -a recursor_ng \
-    -- -q -f run 'recursor_ng pintosmaster 6 1'
+time pintos -v -m 128 -k -T 60 qemu --filesys-size=4  -p ../examples/recursor_ng -a recursor_ng  -- -q -f run 'recursor_ng pintosmaster 6 1'
